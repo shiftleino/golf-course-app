7e1cb763d541cf8b76a84c8d525ac3d9d5d1bb89
shiftleino <>
2022-01-21 11:40:00 +0200

    Created sql schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,36 @@
-CREATE TABLE users (
+CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    role INTEGER NOT NULL
+);
+
+CREATE TABLE Courses (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    holes INTEGER,
+    link TEXT
+);
+
+CREATE TABLE Reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users,
+    course_id INTEGER REFERENCES Courses,
+    rating INTEGER,
+    comment TEXT
+);
+
+CREATE TABLE CoursePrices (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    key TEXT,
+    value INTEGER
+);
+
+CREATE TABLE CourseLocations (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    latitude INTEGER,
+    longitude INTEGER,
+    municipality TEXT
 );
