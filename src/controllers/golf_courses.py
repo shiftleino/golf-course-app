8be6cf3583e0aafa7957558cbbb2f808a4275195
shiftleino <>
2022-01-30 15:43:30 +0200

    Added different sort options into the list

--- src/controllers/golf_courses.py
+++ src/controllers/golf_courses.py
@@ -1,13 +1,28 @@
 from database.db import db
 
-def get_basic_info(order="C.name"):
-    sql = """
-    SELECT C.id, C.name, L.distance, (SELECT value AS greenfee FROM CoursePrices WHERE key='Green fee' AND course_id=C.id)
-    FROM Courses as C, CourseLocations AS L
-    WHERE C.id=L.course_id
-    ORDER BY :order;
-    """
-    result = db.session.execute(sql, {"order": order}).fetchall()
+def get_basic_info(order="name"):
+    if order == "distance":
+        sql = """
+        SELECT C.id, C.name, L.distance, (SELECT value AS greenfee FROM CoursePrices WHERE key='Green fee' AND course_id=C.id)
+        FROM Courses as C, CourseLocations AS L
+        WHERE C.id=L.course_id
+        ORDER BY L.distance;
+        """
+    elif order == "greenfee":
+        sql = """
+        SELECT C.id, C.name, L.distance, (SELECT value AS greenfee FROM CoursePrices WHERE key='Green fee' AND course_id=C.id)
+        FROM Courses as C, CourseLocations AS L
+        WHERE C.id=L.course_id
+        ORDER BY greenfee;
+        """
+    else:
+        sql = """
+        SELECT C.id, C.name, L.distance, (SELECT value AS greenfee FROM CoursePrices WHERE key='Green fee' AND course_id=C.id)
+        FROM Courses as C, CourseLocations AS L
+        WHERE C.id=L.course_id
+        ORDER BY C.name;
+        """
+    result = db.session.execute(sql).fetchall()
     return result
 
 def get_course_info(course_id):
