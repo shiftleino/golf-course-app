0e385b9bff50de66dcf294651e59003f4549d2b2
shiftleino <>
2022-02-14 15:59:57 +0200

    Added timestamp to sql command

--- src/controllers/reviews.py
+++ src/controllers/reviews.py
@@ -3,7 +3,7 @@ from database.db import db
 def add_review(data):
     sql = """
     INSERT INTO Reviews (user_id, course_id, rating, comment, sent_at)
-    VALUES (:user_id, :course_id, :rating, :comment, :sent_at);
+    VALUES (:user_id, :course_id, :rating, :comment, NOW());
     """
     db.session.execute(sql, {"user_id": data.user_id, "course_id": data.course_id, "rating": int(data.rating), "comment": data.comment, "sent_at": data.sent_at})
     db.session.commit()
