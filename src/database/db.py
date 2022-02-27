469e6247a01f6ef7affccb7adc715ee242dd667c
shiftleino <>
2022-02-27 21:45:32 +0200

    Bug fix

--- src/database/db.py
+++ src/database/db.py
@@ -2,7 +2,7 @@ from src.app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
-from app import app
+from src.app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
